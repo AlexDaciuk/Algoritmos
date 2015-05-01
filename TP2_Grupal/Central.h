383af8cd275d077e61a050153082a2c1b73ebf99
Alexis Daciuk <adaciuk@gmail.com>
2015-05-01 15:58:06 -0300

    Agregue un par de metodos a la clase Central

--- TP2_Grupal/Central.h
+++ TP2_Grupal/Central.h
@@ -1,22 +1,31 @@
 /**
- *
- *
- *
+ * Clase que maneja cada central 
  */
 
 
 class Central
 {
 private:
+  int numeroCentral;
+
+  ListaEnlaces* listaEnlaces;
+
+public:
+
+  /**
+   *Constuctor de la clase
+   *Post: Va a quedar creada la central 
+   */
   
+  Central(int numeroCentralTmp);
 
+  void agregarInterno(int numeroInterno);
 
+  void empezarLlamada(int numeroInterno, int numeroReceptorTmp, int centralReceptoraTmp, int horaInicioTmp);
 
+  void cortarLlamada(int numeroInterno, int numeroReceptorTmp, int centralReceptoraTmp, int horaFinTmp);
 
-
-
-
-
+   
 
 
 }
