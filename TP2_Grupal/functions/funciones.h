a0d4b217a669d180c7369f8308e6b5d08ce3d25a
llmanuel <llauromanuel@gmail.com>
2015-05-03 16:22:19 -0300

    Visualisacion para pantallass de 13p jajaja

--- TP2_Grupal/functions/funciones.h
+++ TP2_Grupal/functions/funciones.h
@@ -5,8 +5,10 @@
 
 
 void leerArchivo();
-void procesarLlamadas(std::string accionTemporal, std::string centralATemporal, std::string internoATemporal, std::string centralBTemporal, std::string internoBTemporal, std::string horaTemporal);
-void procesarEnlace(std::string accionTemporal, std::string centralATemporal, std::string centralBTemporal, std::string distanciaKmTemporal, std::string precioMinutoTemporal, std::string cantidadCanalesTemporal);
+void procesarLlamadas(std::string accionTemporal, std::string centralATemporal, std::string internoATemporal,
+					  std::string centralBTemporal, std::string internoBTemporal, std::string horaTemporal);
+void procesarEnlace(std::string accionTemporal, std::string centralATemporal, std::string centralBTemporal, 
+					std::string distanciaKmTemporal, std::string precioMinutoTemporal, std::string cantidadCanalesTemporal);
 void menu();
 
 #endif
