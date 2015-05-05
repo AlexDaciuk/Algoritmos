145acbd6fda4afd8d64f0ad55818e7b62e61e701
llmanuel <llauromanuel@gmail.com>
2015-05-05 13:34:54 -0300

    Arreglo Internos.h

--- TP2_Grupal/class/Internos.h
+++ TP2_Grupal/class/Internos.h
@@ -11,12 +11,7 @@ class Interno
 {
  private:
   int numeroInterno;
-<<<<<<< HEAD
   Llamada* punteroASusLlammadas; //apunta siempre a la primer llamada
-  Interno* internoSiguiente;
-=======
-  Llamada* punteroASusLlammadas;
->>>>>>> 680b1b9e02bdce04c73b3929c196972c9a066993
  public:
 
   /**
