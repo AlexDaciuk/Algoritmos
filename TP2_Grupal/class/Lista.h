b5a3a226872f3b38723b3dfd334490d30a2d443e
llmanuel <llauromanuel@gmail.com>
2015-05-09 16:53:13 -0300

    lista.h

---
+++ TP2_Grupal/class/Lista.h
@@ -0,0 +1,21 @@
+#ifndef LISTA_H
+#define LIsTA_H
+#include <iostream>
+
+template <class T>
+class Lista
+{
+  private:
+  
+    Nodo<T>* primerNodo;
+    Nodo<T>* segundoNodo;
+    
+  public:
+  
+    Lista();
+    void Insertar( T dato);
+    void Borrar(int codigo);//El codigo con el que identificamos al objeto en la lista
+    void PonerPrimero(int codigo);
+};
+
+#endif
