6edca24bce42d2a6cb1d8168f00f3435798a10aa
llmanuel <llauromanuel@gmail.com>
2015-05-09 17:54:50 -0300

    agregado de insertar

--- TP2_Grupal/class/Lista.h
+++ TP2_Grupal/class/Lista.h
@@ -1,9 +1,10 @@
 #ifndef LISTA_H
 #define LIsTA_H
+#define NULL=0
 #include <iostream>
 
 template <class T>
-class Lista
+class Lista<T>
 {
   private:
   
@@ -13,9 +14,34 @@ class Lista
   public:
   
     Lista();
-    void Insertar( T dato);
+    void Insertar( T objeto);
     void Borrar(int codigo);//El codigo con el que identificamos al objeto en la lista
     void PonerPrimero(int codigo);
 };
+
+template <class T>
+Lista::Lista()
+{
+  this->primerNodo=NULL;
+  this->ultimoNodo=NULL;
+};
+
+template <class T>
+void Lista::Insertar( T objeto)
+{
+  Nodo<T>* nuevoNodo= new Nodo<T>(objeto);
+  if (ultimoNodo!=NULL)
+  {
+    nuevoNodo->cambiarNodoSiguiente(this->primerNodo);
+    this->primerNodo->cambiarNodoAnterior(nuevoNodo);
+    this->primerNodo=nuevoNodo;
+  }
+  else
+  {
+    this->primerNodo=nuevoNodo;
+    this->ultimoNodo=nuevoNodo;
+  }
+};
+
 
 #endif
