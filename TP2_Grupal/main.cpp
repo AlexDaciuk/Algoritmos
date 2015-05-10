5fbd700e95e228266767fedd7f5b8de7d2b3bd4f
Alexis Daciuk <adaciuk@gmail.com>
2015-05-10 15:35:18 -0300

    Agrego la parte que pregunta que algorimo usar

--- TP2_Grupal/main.cpp
+++ TP2_Grupal/main.cpp
@@ -13,6 +13,33 @@
 
 int main()
 {
+
+  std::string opcionAlgoritmo;
+
+
+  cout << "Quiere usar algoritmo de: \n";
+  cout << "1) Precio mas bajo \n";
+  cout << "2) Distancia mas corta \n";
+
+  int opcionTmp;
+
+  cin >> opcionTmp;
+
+  
+  if ( opcionTmp == 1)
+    {
+      opcionAlgoritmo = "Precio";
+    }
+  else if ( opcionTmp == 2)
+    {
+      opcionAlgoritmo = "Distancia";
+    }
+
+  
+  
+  
+
+
   
   return 0;
 }
