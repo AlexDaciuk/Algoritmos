3c9d8864c267fd55564a3903ae8dc8dfcb6fe8b3
Lucas Risaro <lucas-nahuel@hotmail.com>
2015-05-16 15:47:12 -0300

    Correccion de la implementacion.

--- TP2_Grupal/class/Nodo.h
+++ TP2_Grupal/class/Nodo.h
@@ -24,68 +24,79 @@ public:
    * Pre: Se le debe pasar un objeto del tipo correspondiente al tipo que guarda el nodo.
    * Post: El nodo se crea con un dato almacenado y sus punteros apuntando a null
    */ 
+  Nodo(T objeto);
+  /**
+   * Post: Cambia la dierccion del puntero que apunta al siguente nodo
+   */
   
-  Nodo(T objeto)  {
+  void cambiarNodoSiguiente(Nodo<T>* nodoSiguiente);
+  /**
+   * Post: Cambia la direccion del puntero que apunta al nodo anterior
+   */
+  
+  void cambiarNodoAnterior(Nodo<T>* nodoAnterior);
+   /**
+   * Pre: El objeto que se pasa tiene que ser el mismo que el del noto
+   * Post: El objeto original queda reemplazado por el nuevo
+   */
+  
+  void cambiarObjeto(T objeto);
+  
+  /**
+   * Post: Devuelve la direccion de memoria del proximo nodo en la lista
+   */
+  
+  Nodo<T>* obtenerSiguiente() ;
+  
+  /**
+   * Post: Devuelve la direccion de memoria del nodo anterior en la lista
+   */
+  
+  Nodo<T>* obtenerAnterior();
+  
+  /**
+   * Post: Devuelve el objeto guardado en el nodo
+   */
+  
+  T obtenerObjeto();
+};
+  
+  
+  template <class T> Nodo(T objeto)  {
 
     this-> objeto= objeto;
     this-> nodoSiguiente= NULL;
     this-> nodoAnterior= NULL;
   }
   
-
-  /**
-   * Post: Cambia la dierccion del puntero que apunta al siguente nodo
-   */
-  
-  void cambiarNodoSiguiente(Nodo<T>* nodoSiguiente)  {
+   template <class T> void Nodo<T>::cambiarNodoSiguiente(Nodo<T>* nodoSiguiente)  {
 
     this->nodoSiguiente=nodoSiguiente;
   }  
-
-  /**
-   * Post: Cambia la direccion del puntero que apunta al nodo anterior
-   */
   
-  void cambiarNodoAnterior(Nodo<T>* nodoAnterior)  {
+  template <class T> void Nodo<T>::cambiarNodoAnterior(Nodo<T>* nodoAnterior)  {
 
     this->nodoAnterior=nodoAnterior;
   } 
   
-  /**
-   * Pre: El objeto que se pasa tiene que ser el mismo que el del noto
-   * Post: El objeto original queda reemplazado por el nuevo
-   */
-  
-  void cambiarObjeto(T objeto){
+  template <class T> void Nodo<T>::cambiarObjeto(T objeto){
     this->objeto=objeto;
-  } 
-
-  /**
-   * Post: Devuelve la direccion de memoria del proximo nodo en la lista
-   */
+  }
   
-  Nodo<T>* obtenerSiguiente()  {
+  template <class T> Nodo<T>* Nodo<T>::obtenerSiguiente()  {
     
     return this->nodoSiguiente;
   }
   
-  /**
-   * Post: Devuelve la direccion de memoria del nodo anterior en la lista
-   */
-  
-  Nodo<T>* obtenerAnterior()  {
+  template <class T> Nodo<T>* Nodo<T>::obtenerAnterior()  {
     
     return this->nodoAnterior;
-  } 
+  }
   
-  /**
-   * Post: Devuelve el objeto guardado en el nodo
-   */
-  
-  T obtenerObjeto() {
+  template <class T> T Nodo<T>::obtenerObjeto() {
     return this->objeto;
   }
-};
+
 
 #endif
 
