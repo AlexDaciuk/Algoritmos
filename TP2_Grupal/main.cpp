cdfebf246868c52524a4953b0d44644c795d1415
LucasRisaro <lucas-nahuel@hotmail.com>
2015-05-17 18:18:45 -0300

    Correccion en los numeros de las opciones y descomento del procesador de llamadas.

--- TP2_Grupal/main.cpp
+++ TP2_Grupal/main.cpp
@@ -35,41 +35,39 @@ int main()
       break;
     }
 
-  /**
-   * Proceso el archivo
-   */
 
-  std::cout<<"Ingrese la ruta del archivo de llamadas.\n";
+  std::std::cout<<"Ingrese la ruta del archivo de llamadas.\n";
   std::string ruta;
 
-  // ProcesadorLlamada procesadorDeLlamadas;
+  ProcesadorLlamada procesadorDeLlamadas;
   procesadorDeLlamadas.procesarLlamadas(ruta);
 
-  std::cout<<"Menu de opciones:\n\n";
-  cout<<"0)Imprimir detalle de internos, nombres y números.\n";
-  cout<<"1)Imprimir detalle de centrales con su numeración.\n";
-  cout<<"2)Imprimir detalle de enlaces (origen, destino, canales). \n";
-  cout<<"3)Detalle de llamadas entre el interno 'X' de la central 'A'"; 
-  cout<<"al interno 'Y' de la central 'B'.\n";
+  std::cout<<"Bienvenido al sistema de informacion de la central telefonica.\n \n";
+	std::cout<<"A continuacion de detalla el menu, elija la que desee.\n\n";
+  std::cout<<"1)Imprimir detalle de internos, nombres y números.\n";
+  std::cout<<"2)Imprimir detalle de centrales con su numeración.\n";
+  std::cout<<"3)Imprimir detalle de enlaces (origen, destino, canales). \n";
+  std::cout<<"4)Detalle de llamadas entre el interno 'X' de la central 'A'"; 
+  std::cout<<"al interno 'Y' de la central 'B'.\n";
   
-  cout<<"4)Interno que mas hablo de todos y de cada central.\n";
-  cout<<"5)Interno que más llamó de todos y de cada central.\n";
-  cout<<"6) Interno que mas le dio ocupado sus intentos y de cada central.\n";
-  cout<<"7)Interno al que más llamaron de todos y de cada central.\n";
-  cout<<"8)Interno que mas gasto de todos y de cada central.\n";
-  cout<<"9)Interno al que más le hablaron de todos y de cada central.\n";
-  cout<<"10)Interno que más dio ocupado de todos y de cada central.\n";
-  cout<<"11)Detalle de llamadas emitidas por el interno X de la central A.\n";
-  cout<<"12)Detalle de llamadas recibidas por el interno X de la central A.\n";
-  cout<<"13)Detalle de llamadas realizadas de X de la central A a Y de la Central B.\n";
-  cout<<"14)Detalle de llamadas recibidas por X de la Central A";
-  cout<<"y realizadas por Y de la Central B.\n";
+  std::cout<<"5)Interno que mas hablo de todos y de cada central.\n";
+  std::cout<<"6)Interno que más llamó de todos y de cada central.\n";
+  std::cout<<"7) Interno que mas le dio ocupado sus intentos y de cada central.\n";
+  std::cout<<"8)Interno al que más llamaron de todos y de cada central.\n";
+  std::cout<<"9)Interno que mas gasto de todos y de cada central.\n";
+  std::cout<<"10)Interno al que más le hablaron de todos y de cada central.\n";
+  std::cout<<"11)Interno que más dio ocupado de todos y de cada central.\n";
+  std::cout<<"12)Detalle de llamadas emitidas por el interno X de la central A.\n";
+  std::cout<<"13)Detalle de llamadas recibidas por el interno X de la central A.\n";
+  std::cout<<"14)Detalle de llamadas realizadas de X de la central A a Y de la Central B.\n";
+  std::cout<<"15)Detalle de llamadas recibidas por X de la Central A";
+  std::cout<<"y realizadas por Y de la Central B.\n";
   
-  cout<<"15)Imprimir cantidad de llamadas anuladas por falta de enlaces por central,";
-  cout<<"ordenado por cantidad descendiente.\n"
-  cout>>"16)Salir. \n \n";
+  std::cout<<"16)Imprimir cantidad de llamadas anuladas por falta de enlaces por central,";
+  std::cout<<"ordenado por cantidad descendiente.\n"
+  std::cout>>"17)Salir. \n \n";
   int opcionMenu;
-  cout<<"Opcion: \n\n";
+  std::cout<<"Opcion: \n\n";
   cin>>opcionMenu;
   
   
