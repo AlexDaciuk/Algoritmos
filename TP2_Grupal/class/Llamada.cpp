4cf5c629709f7f92cd73fef95bfb24026e2f3aeb
Alexis Daciuk <adaciuk@gmail.com>
2015-05-24 19:01:05 -0300

    arreglo tabulacion

--- TP2_Grupal/class/Llamada.cpp
+++ TP2_Grupal/class/Llamada.cpp
@@ -8,49 +8,48 @@
 
 Llamada::Llamada (int receptorLlamadaTemporal)
 {
-  if (receptorLlamadaTemporal > 0 && receptorLlamadaTemporal =< 9999)
-    {
-      this->cantidadLlamadas = 0;
-      this->duracionLlamadas = 0;
-      this->cantidadOcupadosDados = 0;
-      this->cantidadOcupadosRecibidos = 0;
-      this->horaInicioLlamadaEnCurso =0;
-      this->receptorLlamada = receptorLlamadaTemporal;
-    }
-  
+	if (receptorLlamadaTemporal > 0 && receptorLlamadaTemporal =< 9999) {
+		this->cantidadLlamadas = 0;
+		this->duracionLlamadas = 0;
+		this->cantidadOcupadosDados = 0;
+		this->cantidadOcupadosRecibidos = 0;
+		this->horaInicioLlamadaEnCurso =0;
+		this->receptorLlamada = receptorLlamadaTemporal;
+	}
+
 }
 
 void Llamada::empezarLlamada(int horaInicioLlamadaEnCursoTemporal)
 {
-  this->cantidadLlamadas++;
-  this->horaInicioLlamadaEnCurso = horaInicioLlamadaEnCursoTemporal;
+	this->cantidadLlamadas++;
+	this->horaInicioLlamadaEnCurso = horaInicioLlamadaEnCursoTemporal;
 }
 
 void Llamada::terminarLlamada (int horaFinLlamadaEnCursoTemporal)
 {
-  this->duracionLlamadas = (horaFinLlamadaEnCursoTemporal - this->horaInicioLlamadaEnCurso) + this-> duracionLlamadas;
-  this->horaInicioLlamadaEnCurso = 0;
+	this->duracionLlamadas = (horaFinLlamadaEnCursoTemporal - this->horaInicioLlamadaEnCurso) + this-> duracionLlamadas;
+	this->horaInicioLlamadaEnCurso = 0;
 }
 
 int Llamada::obtenerReceptorLlamada()
 {
-  return this->receptorLlamada;
-} 
+	return this->receptorLlamada;
+}
 
 int Llamada::obtenerDuracionLlamadas()
 {
-  return this->duracionLlamadas;
+	return this->duracionLlamadas;
 }
 
 int Llamada::obtenerCantidadOcupadosDados()
 {
-  return this->cantidadOcupadosDados;
+	return this->cantidadOcupadosDados;
 }
 
 
 int Llamada::obtenerCantidadOcupadosRecibidos()
 {
-  return this->cantidadOcupadosRecibidos;
+	return this->cantidadOcupadosRecibidos;
 }
 
 #endif
--- TP2_Grupal/class/Llamada.h
+++ TP2_Grupal/class/Llamada.h
@@ -10,44 +10,43 @@ class Llamada
 {
 private:
 
-    int  cantidadLlamadas, duracionLlamadas, cantidadOcupadosDados, cantidadOcupadosRecibidos, horaInicioLlamadaEnCurso, receptorLlamada;
-  
+	int  cantidadLlamadas, duracionLlamadas, cantidadOcupadosDados, cantidadOcupadosRecibidos, horaInicioLlamadaEnCurso, receptorLlamada;
+
 public:
 
-  /**
-   * Constructor de la clase llamada
-   * Post: La llamada queda registrada
-   * Si el receptor no esta en el rango, se deja en 0 el receptor de la llamada
-   */
-  
-  Llamada(int receptorLlamadaTemporal);
-  
-  /*post: guarda la hora de comienzo de la llamada y aumeta el numero de 
-   * llamadas en uno.*/
-  void empezarLlamada(int horaInicioLlamadaEnCursoTemporal);
-  
-  /*pre: la llamada debe haber comenzado antes.
-   * post: calcula la duracion de la llamada y vuelve a poner el horario de inicio
-   * en cero.*/
-  void terminarLlamada(int horaFinLlamadaEnCursoTemporal);
-  
-  /*post: devuelve la hora de inicio de la llamada.*/
-  int obtenerHoraInicioLlamadaEnCurso();
-  
-  /*post: devuelve a que receptor pertenece el nodo de la listas de llamdas
-   * del interno.*/
-  int obtenerReceptorLlamada();
-  
-  /*post: devuelve la duracion acumulada todas las llamadas efectuadas por el interno.*/
-  int obtenerDuracionLlamadas();
-  
-  /*post: devuelve la cantidad total de ocupados dados por el interno.*/
-  int obtenerCantidadOcupadosDados();
-  
-  /*post: devuelve la cantidad total de ocupados recibidos por el interno.*/
-  int obtenerCantidadOcupadosRecibidos();
-     
+	/**
+	 * Constructor de la clase llamada
+	 * Post: La llamada queda registrada
+	 * Si el receptor no esta en el rango, se deja en 0 el receptor de la llamada
+	 */
+
+	Llamada(int receptorLlamadaTemporal);
+
+	/*post: guarda la hora de comienzo de la llamada y aumeta el numero de
+	 * llamadas en uno.*/
+	void empezarLlamada(int horaInicioLlamadaEnCursoTemporal);
+
+	/*pre: la llamada debe haber comenzado antes.
+	 * post: calcula la duracion de la llamada y vuelve a poner el horario de inicio
+	 * en cero.*/
+	void terminarLlamada(int horaFinLlamadaEnCursoTemporal);
+
+	/*post: devuelve la hora de inicio de la llamada.*/
+	int obtenerHoraInicioLlamadaEnCurso();
+
+	/*post: devuelve a que receptor pertenece el nodo de la listas de llamdas
+	 * del interno.*/
+	int obtenerReceptorLlamada();
+
+	/*post: devuelve la duracion acumulada todas las llamadas efectuadas por el interno.*/
+	int obtenerDuracionLlamadas();
+
+	/*post: devuelve la cantidad total de ocupados dados por el interno.*/
+	int obtenerCantidadOcupadosDados();
+
+	/*post: devuelve la cantidad total de ocupados recibidos por el interno.*/
+	int obtenerCantidadOcupadosRecibidos();
+
 };
 
 #endif
-
