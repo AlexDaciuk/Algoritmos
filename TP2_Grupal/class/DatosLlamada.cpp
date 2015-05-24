e260408e6931a52d099553714e5bcadc687f5569
Alexis Daciuk <adaciuk@gmail.com>
2015-05-24 18:57:46 -0300

    Arreglo tabulacion

--- TP2_Grupal/class/DatosLlamada.cpp
+++ TP2_Grupal/class/DatosLlamada.cpp
@@ -2,64 +2,63 @@
 
 DatosLlamada::DatosLlamada()
 {
-  this->accion = " ";
-  this->origen = 0;
-  this->destino = 0;
-  this->emisor = 0;
-  this->receptor = 0;
-  this->hora = 0;
+	this->accion = " ";
+	this->origen = 0;
+	this->destino = 0;
+	this->emisor = 0;
+	this->receptor = 0;
+	this->hora = 0;
 }
 
 
 void DatosLlamada::cargarLlamada(std::string accionTemporal, std::string origenTemporal, std::string emisorTemporal, std::string destinoTemporal, std::string receptorTemporal, std::string horaTemporal)
 {
-  if (! accionTemporal.empty())
-    {
-      this->accion = accionTemporal;
-      
-      std::stringstream origenStream(origenTemporal);
-      origenStream >> this->origen;      
+	if (! accionTemporal.empty()) {
+		this->accion = accionTemporal;
 
-      std::stringstream emisorStream(emisorTemporal);
-      emisorStream >> this->emisor;
-      
-      std::stringstream destinoStream(destinoTemporal);
-      destinoStream >> this->destino;
-    
-      std::stringstream receptorStream(receptorTemporal);
-      receptorStream >> this->receptor;  
-        
-      std::stringstream horaInicioStreamTemporal(horaTemporal);
-      horaInicioStreamTemporal >> this->hora;
-      
-    }
+		std::stringstream origenStream(origenTemporal);
+		origenStream >> this->origen;
+
+		std::stringstream emisorStream(emisorTemporal);
+		emisorStream >> this->emisor;
+
+		std::stringstream destinoStream(destinoTemporal);
+		destinoStream >> this->destino;
+
+		std::stringstream receptorStream(receptorTemporal);
+		receptorStream >> this->receptor;
+
+		std::stringstream horaInicioStreamTemporal(horaTemporal);
+		horaInicioStreamTemporal >> this->hora;
+
+	}
 }
 
 std::string DatosLlamada::obtenerAccion()
 {
-  return this->accion;
+	return this->accion;
 }
 
 int DatosLlamada::obtenerOrigen()
 {
-  return this->origen;
+	return this->origen;
 }
 int DatosLlamada::obtenerDestino()
 {
-  return this->destino;
+	return this->destino;
 }
 
 int DatosLlamada::obtenerEmisor()
 {
-  return this->emisor;
+	return this->emisor;
 }
 
 int DatosLlamada::obtenerReceptor()
 {
-  return this->emisor;
+	return this->emisor;
 }
 
 int DatosLlamada::obtenerHora()
 {
-  return this->hora;
+	return this->hora;
 }
--- TP2_Grupal/class/DatosLlamada.h
+++ TP2_Grupal/class/DatosLlamada.h
@@ -18,38 +18,38 @@
 class DatosLlamada
 {
 private:
-  std::string accion;
-  int origen, emisor, destino, receptor, hora;
-  
+	std::string accion;
+	int origen, emisor, destino, receptor, hora;
+
 
 public:
-  /**
-   * Constructor
-   * Inicializa los enteros en 0 y el string en un espacio
-   */
-  DatosLlamada();
-  
+	/**
+	 * Constructor
+	 * Inicializa los enteros en 0 y el string en un espacio
+	 */
+	DatosLlamada();
 
-  /**
-   * Metodo que carga datos a los atributos, si ya tuviera datos, los sobre escribe
-   * 
-   */
-  
-  void cargarLlamada(std::string accionTemporal, std::string origenTemporal, std::string emisorTemporal,
-		     std::string destinoTemporal, std::string receptorTemporal, std::string horaInicioTemporal);
 
-  /**
-   * Metodos que devuelven los valores de los atributos de la clase
-   */
-  std::string obtenerAccion();
+	/**
+	 * Metodo que carga datos a los atributos, si ya tuviera datos, los sobre escribe
+	 *
+	 */
 
-  int obtenerOrigen();
-  int obtenerDestino();
-  
-  int obtenerEmisor();
-  int obtenerReceptor();
+	void cargarLlamada(std::string accionTemporal, std::string origenTemporal, std::string emisorTemporal,
+	                   std::string destinoTemporal, std::string receptorTemporal, std::string horaInicioTemporal);
 
-  int obtenerHora();
+	/**
+	 * Metodos que devuelven los valores de los atributos de la clase
+	 */
+	std::string obtenerAccion();
+
+	int obtenerOrigen();
+	int obtenerDestino();
+
+	int obtenerEmisor();
+	int obtenerReceptor();
+
+	int obtenerHora();
 };
 
 
