83aefd7a7804f328225cd7bf4187c9f3c7d2e008
Alexis Daciuk <adaciuk@gmail.com>
2015-06-02 19:05:32 -0300

    Agrego comentarios y arreglo identacion

--- TP2_Grupal/class/Central.h
+++ TP2_Grupal/class/Central.h
@@ -9,79 +9,76 @@
 #include "Enlace.h"
 #include "Interno.h"
 
+class Interno;
+class Enlace;
+
 /**
  * Clase que maneja cada central
  */
-
-class Interno;
-class Enlace;
 
 class Central
 {
 private:
 
 	int numeroCentral;
-
 	Lista<Interno*>* internos;
-	// Puntero a la lista de punteros a los enlaces de la central
 	Lista<Enlace*>* enlaces;
 
 public:
-
 	/**
 	 * Constuctor de la clase
-	 * Post: Va a quedar creada el nodo en la lista de centrales
+	 * Post: Va a quedar creada la central con su lista de internos y enlaces vacias
 	 */
-
 	Central(int numeroCentralTemporal);
 
 
 	/**
-	 * Metodo que crea un interno dentro de la central, para usarse cuando entra una llamada de
-	 * otra central y el interno en cuestion no existe
+	 * Metodo que crea un interno dentro de la central, si ya existe el interno, no hace nada
 	 */
-
 	void crearInterno(int numeroInterno);
 
-	/**
-	 * Precondicion: el interno debe existir.
-   * postcondicion: devuelve el puntero al interno indicado de la lista de internos.
-	 */
 
+	/**
+	 * Precondicion: El interno debe existir.
+	 * Postcondicion: Devuelve el puntero al interno solicitado.
+	 */
 	Interno* obtenerInterno(int numeroInterno);
+
 
 	/**
 	 * Metodo que devuelve la lista de internos
 	 */
-
 	Lista<Interno*>* obtenerInternos();
+
 
 	/**
 	 * Metodo que devuelve la lista de enlaces
 	 */
-
 	Lista<Enlace*>* obtenerEnlaces();
 
+	/**
+	 * Metodo que agrega un enlace a la lista de enlaces de la central
+	 */
 	void agregarEnlace(Enlace* enlaceTemporal);
+
 
 	/**
 	 * Metodo que devuelve el numero de la central
 	 */
-
 	int obtenerNumero();
-  /**
-   * post: devuelve el total de llamadas anuladas de toda la lista de internos.
-   */
-  int obtenerTotalDeLlamadasAnuladasDeInternos(Lista<Interno*>* internos);
+
 
 	/**
-	 * destructor de Central
+	 * Devuelve el total de llamadas anuladas de toda la lista de internos.
+	 */
+	int obtenerTotalDeLlamadasAnuladasDeInternos(Lista<Interno*>* internos);
+
+
+	/**
+	 * Destructor de Central, libera el espacio pedido para la lista de enlaces y de internos
 	 * */
 	~Central();
 
-
 };
-
-
 
 #endif
