7fec0a997a4ff6d9a1f5287c6d6b228e972fe804
Alexis Daciuk <adaciuk@gmail.com>
2015-06-14 21:33:30 -0300

    Arreglo error en delete, quito una clase que ya no existe del makefile

--- TP2_Grupal/class/Interfaz.cpp
+++ TP2_Grupal/class/Interfaz.cpp
@@ -869,7 +869,7 @@ void Interfaz::centralesOrdenadasPorLlamadasAnuladas()
 		std::cout<<"   Total de llamadas anuladas:"<<centralesOrdenadas[i]->obtenerTotalDeLlamadasAnuladasDeInternos(centralesOrdenadas[i]->obtenerInternos())<<"\n\n";
 	}
 
-	delete centralesOrdenadas[] ;
+	delete[] centralesOrdenadas ;
 }
 
 void Interfaz::tratarOpcion(int opcion)
